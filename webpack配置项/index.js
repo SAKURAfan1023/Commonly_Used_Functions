f6e1bcf536a570fee95ad5fd182a60878952768e
SAKURA <1023lifanhu@gmail.com>
2025-04-12 22:39:22 +0800

    完善了git文件

--- webpack配置项/index.js
+++ webpack配置项/index.js
@@ -3,3 +3,12 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 console.log('开发模式下可用，生产模式下不可用');
+
+
+
+
+// <% if(HtmlWebpackPlugin.options.useCdn{)%>
+
+//   link标签或者script标签想要调用cdn库的情况
+
+//   <%}/%>
