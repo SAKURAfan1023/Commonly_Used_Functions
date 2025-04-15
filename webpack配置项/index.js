2f9debb3748c1b7a78326a3f64ffe26150d921b8
SAKURA <1023lifanhu@gmail.com>
2025-04-15 17:59:50 +0800

    添加了断点设备查询

--- webpack配置项/index.js
+++ webpack配置项/index.js
@@ -7,8 +7,10 @@ console.log('开发模式下可用，生产模式下不可用');
 
 
 
-// <% if(HtmlWebpackPlugin.options.useCdn{)%>
+// <% if(htmlWebpackPlugin.options.useCdn){ %>
 
 //   link标签或者script标签想要调用cdn库的情况
 
-//   <%}/%>
+//   <% } %>
+
+// font - family: -apple - system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, sans - serif;
--- webpack配置项/webpack.config.cjs
+++ webpack配置项/webpack.config.cjs
@@ -130,7 +130,7 @@ const config = {
                 }
               ]
             },
-            esModule: false
+            esModule: false,
           }
         }
       },
